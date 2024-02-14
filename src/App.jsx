d142a4ff4fba25c9539dc975cb530b1ab6337e22
julius <nyerereoffice@gmail.com>
2024-02-14 11:28:10 +0300

    A React Accordion project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Accordion from './components/accordion'
+
+function App() {
+
+  return (
+    <div className='App'>
+      {/* This is my component */}
+      <Accordion/>
+      
+    </div>
+  )
+}
+
+export default App
